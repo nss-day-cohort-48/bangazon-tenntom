f6b59fbde829d5b3cfd3ae46df327ace1867b0ab
Tom Clouse <tommyclouse@yahoo.com>
2021-09-02 15:51:18 -0500

    still working on it

--- bangazonapi/views/order.py
+++ bangazonapi/views/order.py
@@ -106,8 +106,7 @@ class Orders(ViewSet):
         order = Order.objects.get(pk=pk, customer=customer)
         order_payment = Payment.objects.get(pk=request.data["payment_type_id"])
         order.payment_type = order_payment
-        # order_products = Product.objects.get(pk=request.data['product_id'])
-        # order.products = order_products
+
         order.save()
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
